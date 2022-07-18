69f1451e0cd76eb1f9c9cd31218008f60660b571
Kristina Petrova <kristina.petrova13@gmail.com>
2022-07-18 15:57:54 +0100

    implement score

--- sketch.js
+++ sketch.js
@@ -10,17 +10,16 @@ var gameOverMusic;
 var jumpSound;
 var gameOver = false;
 var platformsGroup;
-var gravity = 1
-var jumpPower = 15
-var runnerSpeed = 12
-var currentBackgroundTilePosition
-var backgroundTiles
-const height = 390
-const width = 840
+var gravity = 1;
+var jumpPower = 15;
+var runnerSpeed = 12;
+var currentBackgroundTilePosition;
+var backgroundTiles;
+var playerScore = 0;
+const height = 390;
+const width = 840;
 
-var currentPlatformLocation = -width
-
-
+var currentPlatformLocation = -width;
 
 function preload() {
   jumpingAnimation = loadAnimation(
@@ -67,128 +66,162 @@ function preload() {
 function setup() {
   createCanvas(width, height);
   runner = createSprite(50, 100, 25, 40);
-  runner.depth = 4
+  runner.depth = 4;
   runner.addAnimation("jump", jumpingAnimation);
   runner.addAnimation("run", runningAnimation);
   runner.setCollider("rectangle", 0, 0, 10, 41);
-  platformsGroup = new Group;
-  backgroundTiles = new Group;
-  currentBackgroundTilePosition = -width
+  platformsGroup = new Group();
+  backgroundTiles = new Group();
+  currentBackgroundTilePosition = -width;
 }
-
 
 function draw() {
   if (!gameOver) {
     background(200);
-    runner.velocity.y += gravity
-    runner.velocity.x = runnerSpeed
-    runner.collide(platformsGroup, solidGround)
-    addNewPlatforms()
-    jumpDetection()
-    camera.position.x = runner.position.x + 300
-    console.log(runner.velocity.y)
-    removeOldPlatforms()
-    addNewBackgroundTiles()
-    removeOldBackgroundTiles()
-    fallCheck()
+
+    runner.velocity.y += gravity;
+    runner.velocity.x = runnerSpeed;
+    runner.collide(platformsGroup, solidGround);
+    addNewPlatforms();
+    jumpDetection();
+
+    camera.position.x = runner.position.x + 300;
+    removeOldPlatforms();
+    addNewBackgroundTiles();
+    removeOldBackgroundTiles();
+    fallCheck();
     drawSprites();
+    updateScore();
   }
   if (gameOver) {
-    gameOverText()
-    updateSprites(false)
+    gameOverText();
+    updateSprites(false);
     if (keyWentDown("space")) {
-      newGame()}
+      newGame();
+    }
   }
   addNewPlatforms();
 }
 
-function fallCheck(){
-  if (runner.position.y > height){
-    gameOver = true
+function updateScore() {
+  if (frameCount % 60 === 0) {
+    playerScore++;
+  }
+  console.log(playerScore);
 
+  fill("white");
+  textFont(gameFont);
+  strokeWeight(2);
+  stroke("black");
+  textSize(20);
+  textAlign(CENTER);
+  text(playerScore, camera.position.x + 350, camera.position.y + 160);
+}
+
+function fallCheck() {
+  if (runner.position.y > height) {
+    gameOver = true;
   }
 }
 
-function gameOverText(){
+function gameOverText() {
   background(0, 0, 0, 10);
-  fill('white')
-  stroke('black')
-  textAlign(CENTER)
-  textFont(gameFont)
+  fill("white");
+  stroke("black");
+  textAlign(CENTER);
+  textFont(gameFont);
 
-  strokeWeight(2)
-  textSize(90)
-  strokeWeight(10)
-  text("GAME OVER", camera.position.x, camera.position.y)
-  textSize(15)
-  text("Press space to try again", camera.position.x, camera.position.y + 100)
+  strokeWeight(2);
+  textSize(90);
+  strokeWeight(10);
+  text("GAME OVER", camera.position.x, camera.position.y);
+  textSize(15);
+  text("Press space to try again", camera.position.x, camera.position.y + 100);
+  textSize(20);
+  text(
+    "You scored " + playerScore + " points!",
+    camera.position.x,
+    camera.position.y + 50
+  );
 }
 
-
-
-function addNewBackgroundTiles(){
-  if (backgroundTiles.length < 3){
-    currentBackgroundTilePosition += 839
-    let bgLoop = createSprite(currentBackgroundTilePosition, height/2, width,height)
-    bgLoop.addAnimation("bg", gameBackground)
-    bgLoop.depth = 1
-    backgroundTiles.add(bgLoop)
+function addNewBackgroundTiles() {
+  if (backgroundTiles.length < 3) {
+    currentBackgroundTilePosition += 839;
+    let bgLoop = createSprite(
+      currentBackgroundTilePosition,
+      height / 2,
+      width,
+      height
+    );
+    bgLoop.addAnimation("bg", gameBackground);
+    bgLoop.depth = 1;
+    backgroundTiles.add(bgLoop);
   }
 }
-function removeOldBackgroundTiles(){
-  for (let i = 0; i < backgroundTiles.length; i++){
-    if (backgroundTiles[i].position.x < runner.position.x-width){
+function removeOldBackgroundTiles() {
+  for (let i = 0; i < backgroundTiles.length; i++) {
+    if (backgroundTiles[i].position.x < runner.position.x - width) {
       backgroundTiles[i].remove();
     }
-  }}
-  
-  function removeOldPlatforms(){
-    for(let i = 0; i < platformsGroup.length; i++){
-      if ((platformsGroup[i].position.x) < runner.position.x-width){
-        platformsGroup[i].remove();
-      }
+  }
+}
+
+function removeOldPlatforms() {
+  for (let i = 0; i < platformsGroup.length; i++) {
+    if (platformsGroup[i].position.x < runner.position.x - width) {
+      platformsGroup[i].remove();
     }
   }
-  
-  function addNewPlatforms(){
-    if (platformsGroup.length < 5){
-      let currentPlatformLength = 1132
-      let platform = createSprite((currentPlatformLocation * 1.2), random(300,400), 1132, 336)
-      platform.collide(runner)
-      currentPlatformLocation += currentPlatformLength
-      platform.addAnimation("default", platformBackground)
-      platform.depth = 3
-      platformsGroup.add(platform)
-    }
+}
+
+function addNewPlatforms() {
+  if (platformsGroup.length < 5) {
+    let currentPlatformLength = 1132;
+    let platform = createSprite(
+      currentPlatformLocation * 1.2,
+      random(300, 400),
+      1132,
+      336
+    );
+    platform.collide(runner);
+    currentPlatformLocation += currentPlatformLength;
+    platform.addAnimation("default", platformBackground);
+    platform.depth = 3;
+    platformsGroup.add(platform);
   }
-  
-  function solidGround(){
-    runner.velocity.y = 0
-    runner.changeAnimation("run")
-    if(runner.touching.right){
-      runner.velocity.x = 0
-      runner.velocity.y+=30
-    }
+}
+
+function solidGround() {
+  runner.velocity.y = 0;
+  runner.changeAnimation("run");
+  if (runner.touching.right) {
+    runner.velocity.x = 0;
+    runner.velocity.y += 30;
   }
-  
-  function jumpDetection(){
-    if(keyWentDown(UP_ARROW) && (runner.velocity.y == 0 || runner.velocity.y == 1)){
-      runner.changeAnimation("jump")
-      runner.animation.rewind()
-      runner.velocity.y = -jumpPower
-    }
+}
+
+function jumpDetection() {
+  if (
+    keyWentDown(UP_ARROW) &&
+    (runner.velocity.y == 0 || runner.velocity.y == 1)
+  ) {
+    runner.changeAnimation("jump");
+    runner.animation.rewind();
+    runner.velocity.y = -jumpPower;
   }
-  
-  
-  function newGame(){
-    platformsGroup.removeSprites()
-    backgroundTiles.removeSprites()
-    gameOver = false
-    updateSprites(true)
-    runnerSpeed=12
-    runner.position.x = 50
-    runner.position.y = 100
-    runner.velocity.x = runnerSpeed
-    currentPlatformLocation = -width
-    currentBackgroundTilePosition = -width
-  }
+}
+
+function newGame() {
+  platformsGroup.removeSprites();
+  backgroundTiles.removeSprites();
+  gameOver = false;
+  playerScore = 0;
+  updateSprites(true);
+  runnerSpeed = 12;
+  runner.position.x = 50;
+  runner.position.y = 100;
+  runner.velocity.x = runnerSpeed;
+  currentPlatformLocation = -width;
+  currentBackgroundTilePosition = -width;
+}
